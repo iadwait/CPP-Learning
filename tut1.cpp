6c3178a235b1bf3e43e4b84e374e7eaccdc50533
iadwait <adwaitbarkale@gmail.com>
2024-02-18 13:25:17 +0530

    Added Comments

--- tut1.cpp
+++ tut1.cpp
@@ -1,9 +1,10 @@
-#include<iostream>
-using namespace std;
+// Basic Structure of C++ Program
+#include<iostream> // Header File
+using namespace std; // namespace so we can avoid writing std::cout
 
-int main() {
+int main() { // Start Point is main
     cout<< "-------Execution Starts--------\n";
     cout<< "Hello World";
     cout<< "\n-------Execution Ends--------";
-    return 0;
+    return 0; // Successful program termination
 }
