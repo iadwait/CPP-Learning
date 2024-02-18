3dd979a927488d7271f0bd23e74f8342d36aacde
iadwait <adwaitbarkale@gmail.com>
2024-02-18 12:58:32 +0530

    First Program

---
+++ tut1.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    cout<< "-------Execution Starts--------\n";
+    cout<< "Hello World";
+    cout<< "\n-------Execution Ends--------";
+    return 0;
+}
