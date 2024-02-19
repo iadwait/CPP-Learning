b47b6d8cd6d74007da3497adfa956ddff471493e
iadwait <adwaitbarkale@gmail.com>
2024-02-19 14:06:00 +0530

    Open program as an executable file

--- tut5.cpp
+++ tut5.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<stdlib.h> // For system("pause");
 using namespace std;
 
 int main()
@@ -10,7 +11,8 @@ int main()
     cout<<"Enter the value of Number 2 :\n";
     cin>>num2;
 
-    cout<<"Sum of Number 1 and Number 2 = " <<num1 + num2;
+    cout<<"Sum of Number 1 and Number 2 = " <<num1 + num2 <<endl;
 
+    system("pause"); // Run program directly as a windows application
     return 0;
 }
